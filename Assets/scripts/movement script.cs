82943d4355b4e3c9972c224e310a08eadb183adc
Octupi674 <octupi674@gmail.com>
2026-01-14 10:11:40 +0000

    noah rice coppied alll my code

--- Assets/scripts/movement script.cs
+++ Assets/scripts/movement script.cs
@@ -7,7 +7,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float JumpForce = 5f;
     public float Xvalue = 0.075f;
     bool isGrounded;
-    public float maxSpeed = 0.075f;
+    public float maxSpeed;
+    public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,22 +23,42 @@ public class NewMonoBehaviourScript : MonoBehaviour
         {
             isGrounded = false;
             Debug.Log("Jumped");
-            rb.AddForce(0,JumpForce,0 * JumpForce, ForceMode.Impulse);
+            rb.AddForce(0,JumpForce,0, ForceMode.Impulse);
         }
         
-        if(Input.GetKey("d") && maxSpeed> Vector3.Magnitude)
+        if(Input.GetKey("d"))
         {
             Debug.Log("Moved Right");
-            rb.AddForce(Xvalue * Time.deltaTime,0,0, ForceMode.Impulse);
+            rb.AddForce(Xvalue ,0,0, ForceMode.Impulse);
         }
         
-        if(Input.GetKey("a") && maxSpeed> rb.Vector3.Left)
+        if(Input.GetKey("a"))
         {
             Debug.Log("Moved Left");
-            rb.AddForce(-Xvalue * Time.deltaTime,0,0, ForceMode.Impulse);
+            rb.AddForce(-Xvalue ,0,0, ForceMode.Impulse);
         }
         
-        
+        if (rb.linearVelocity.x > -maxSpeed)
+        {
+            speed = 25;
+        }
+        else
+        {
+            speed = 5;
+            Debug.Log ("lowering speed");
+            rb.AddForce(Vector3.left * -1f, ForceMode.Impulse);
+        }
+
+        if (rb.linearVelocity.x < maxSpeed)
+        {
+            speed = 25;
+        }
+        else
+        {
+            speed = 5;
+            Debug.Log ("lowering speed");
+            rb.AddForce(Vector3.right * -1f, ForceMode.Impulse);
+        }
     }
 
     void OnCollisionStay(Collision other)
